3fadc5e7d3aaee90b98447b8f7fb458f7c8eddd8
Muspah <simondw@sapo.pt>
2024-12-22 14:58:58 +0100

    First try on fixing deployment

--- .github/deploy-staging.sh
+++ .github/deploy-staging.sh
@@ -4,58 +4,58 @@ echo -e "Running staging script...\n"
 echo -e "Publishing javadocs and artifacts...\n"
 cd $HOME
 
-rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+rsync -r --quiet -e "ssh -p 7685 -o StrictHostKeyChecking=no" \
 $HOME/work/uSkyBlock/uSkyBlock/po-utils/target/mvn-repo/ \
-travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
+u36810p330294@uskyblock.ovh:domains/uskyblock.ovh/public_html/maven/uskyblock/
 
-rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+rsync -r --quiet -e "ssh -p 7685 -o StrictHostKeyChecking=no" \
 $HOME/work/uSkyBlock/uSkyBlock/target/mvn-repo/ \
-travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
+u36810p330294@uskyblock.ovh:domains/uskyblock.ovh/public_html/maven/uskyblock/
 
-rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+rsync -r --quiet -e "ssh -p 7685 -o StrictHostKeyChecking=no" \
 $HOME/work/uSkyBlock/uSkyBlock/uSkyBlock-API/target/mvn-repo/ \
-travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
+u36810p330294@uskyblock.ovh:domains/uskyblock.ovh/public_html/maven/uskyblock/
 
-rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+rsync -r --quiet -e "ssh -p 7685 -o StrictHostKeyChecking=no" \
 $HOME/work/uSkyBlock/uSkyBlock/uSkyBlock-APIv2/target/mvn-repo/ \
-travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
+u36810p330294@uskyblock.ovh:domains/uskyblock.ovh/public_html/maven/uskyblock/
 
-rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+rsync -r --quiet -e "ssh -p 7685 -o StrictHostKeyChecking=no" \
 $HOME/work/uSkyBlock/uSkyBlock/uSkyBlock-Core/target/mvn-repo/ \
-travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
+u36810p330294@uskyblock.ovh:domains/uskyblock.ovh/public_html/maven/uskyblock/
 
-rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+rsync -r --quiet -e "ssh -p 7685 -o StrictHostKeyChecking=no" \
 $HOME/work/uSkyBlock/uSkyBlock/uSkyBlock-FAWE/target/mvn-repo/ \
-travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
+u36810p330294@uskyblock.ovh:domains/uskyblock.ovh/public_html/maven/uskyblock/
 
-rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+rsync -r --quiet -e "ssh -p 7685 -o StrictHostKeyChecking=no" \
 $HOME/work/uSkyBlock/uSkyBlock/uSkyBlock-Plugin/target/mvn-repo/ \
-travis@travis.internetpolice.eu:WWW-USB/maven/uskyblock/
+u36810p330294@uskyblock.ovh:domains/uskyblock.ovh/public_html/maven/uskyblock/
 
 echo -e "Publishing javadocs...\n"
 
-rsync -r --delete --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+rsync -r --delete --quiet -e "ssh -p 7685 -o StrictHostKeyChecking=no" \
 $HOME/work/uSkyBlock/uSkyBlock/po-utils/target/site/apidocs/ \
-travis@travis.internetpolice.eu:WWW-USB/javadocs/master/po-utils/
+u36810p330294@uskyblock.ovh:domains/uskyblock.ovh/public_html/javadocs/master/po-utils/
 
-rsync -r --delete --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+rsync -r --delete --quiet -e "ssh -p 7685 -o StrictHostKeyChecking=no" \
 $HOME/work/uSkyBlock/uSkyBlock/uSkyBlock-API/target/site/apidocs/ \
-travis@travis.internetpolice.eu:WWW-USB/javadocs/master/uSkyBlock-API/
+u36810p330294@uskyblock.ovh:domains/uskyblock.ovh/public_html/javadocs/master/uSkyBlock-API/
 
-rsync -r --delete --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+rsync -r --delete --quiet -e "ssh -p 7685 -o StrictHostKeyChecking=no" \
 $HOME/work/uSkyBlock/uSkyBlock/uSkyBlock-APIv2/target/site/apidocs/ \
-travis@travis.internetpolice.eu:WWW-USB/javadocs/master/uSkyBlock-APIv2/
+u36810p330294@uskyblock.ovh:domains/uskyblock.ovh/public_html/javadocs/master/uSkyBlock-APIv2/
 
-rsync -r --delete --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+rsync -r --delete --quiet -e "ssh -p 7685 -o StrictHostKeyChecking=no" \
 $HOME/work/uSkyBlock/uSkyBlock/uSkyBlock-Core/target/site/apidocs/ \
-travis@travis.internetpolice.eu:WWW-USB/javadocs/master/uSkyBlock-Core/
+u36810p330294@uskyblock.ovh:domains/uskyblock.ovh/public_html/javadocs/master/uSkyBlock-Core/
 
 echo -e "Publishing final plugin release...\n"
 
-rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+rsync -r --quiet -e "ssh -p 7685 -o StrictHostKeyChecking=no" \
 $HOME/work/uSkyBlock/uSkyBlock/uSkyBlock-Plugin/target/uSkyBlock-*.jar \
-travis@travis.internetpolice.eu:WWW-USB/downloads/master/uSkyBlock/
+u36810p330294@uskyblock.ovh:domains/uskyblock.ovh/public_html/downloads/master/uSkyBlock/
 
-rsync -r --quiet --no-R --no-implied-dirs -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+rsync -r --quiet --no-R --no-implied-dirs -e "ssh -p 7685 -o StrictHostKeyChecking=no" \
 $HOME/work/uSkyBlock/uSkyBlock/uSkyBlock-Plugin/target/classes/version.json \
-travis@travis.internetpolice.eu:WWW-USB/versions/staging.json
+u36810p330294@uskyblock.ovh:domains/uskyblock.ovh/public_html/versions/staging.json
