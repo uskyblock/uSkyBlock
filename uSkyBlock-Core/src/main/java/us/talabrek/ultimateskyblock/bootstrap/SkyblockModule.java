56c3d59ac4bf6fc6e934f720d5375eb91ab86641
minoneer <minoneer@gmail.com>
2025-02-05 14:00:43 +0100

    Add guice for dependency injection

---
+++ uSkyBlock-Core/src/main/java/us/talabrek/ultimateskyblock/bootstrap/SkyblockModule.java
@@ -0,0 +1,10 @@
+package us.talabrek.ultimateskyblock.bootstrap;
+
+import com.google.inject.AbstractModule;
+
+public class SkyblockModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+    }
+}
