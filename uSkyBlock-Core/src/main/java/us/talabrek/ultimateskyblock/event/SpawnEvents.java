4d7afecca23c7c7946b43c000aaf66b4fe4f9dba
minoneer <minoneer@gmail.com>
2025-02-07 21:06:09 +0100

    Fix #78 UnsupportedOperationException when using a spawner egg

--- uSkyBlock-Core/src/main/java/us/talabrek/ultimateskyblock/event/SpawnEvents.java
+++ uSkyBlock-Core/src/main/java/us/talabrek/ultimateskyblock/event/SpawnEvents.java
@@ -2,8 +2,11 @@ package us.talabrek.ultimateskyblock.event;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
 import org.bukkit.World;
 import org.bukkit.block.Biome;
+import org.bukkit.entity.EntitySnapshot;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Phantom;
 import org.bukkit.entity.Player;
@@ -17,12 +20,15 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SpawnEggMeta;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import us.talabrek.ultimateskyblock.api.IslandInfo;
 import us.talabrek.ultimateskyblock.handler.WorldGuardHandler;
 import us.talabrek.ultimateskyblock.uSkyBlock;
 import us.talabrek.ultimateskyblock.util.LocationUtil;
 
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
 
 import static dk.lockfuglsang.minecraft.po.I18nUtil.tr;
@@ -54,20 +60,35 @@ public class SpawnEvents implements Listener {
             return;
         }
         ItemStack item = event.getItem();
-        if (RIGHT_CLICKS.contains(event.getAction()) && item != null && item.getItemMeta() instanceof SpawnEggMeta spawnEggMeta) {
+        if (RIGHT_CLICKS.contains(event.getAction()) && item != null && item.getItemMeta() instanceof SpawnEggMeta) {
             if (!plugin.playerIsOnIsland(player)) {
                 event.setCancelled(true);
                 plugin.notifyPlayer(player, tr("\u00a7eYou can only use spawn-eggs on your own island."));
                 return;
             }
 
-            //noinspection deprecation
-            checkLimits(event, spawnEggMeta.getSpawnedType(), player.getLocation());
+            checkLimits(event, getSpawnEggType(item), player.getLocation());
             if (event.useItemInHand() == Event.Result.DENY) {
                 plugin.notifyPlayer(player, tr("\u00a7cYou have reached your spawn-limit for your island."));
                 event.setUseItemInHand(Event.Result.DENY);
                 event.setUseInteractedBlock(Event.Result.DENY);
             }
+        }
+    }
+
+    private static @Nullable EntityType getSpawnEggType(@NotNull ItemStack itemStack) {
+        if (itemStack.getItemMeta() instanceof SpawnEggMeta spawnEggMeta) {
+            EntitySnapshot spawnedEntity = spawnEggMeta.getSpawnedEntity();
+            if (spawnedEntity != null) {
+                return spawnedEntity.getEntityType();
+            } else {
+                String key = itemStack.getType().getKey().toString();
+                String entityKey = key.replace("_spawn_egg", "");
+                NamespacedKey namespacedKey = Objects.requireNonNull(NamespacedKey.fromString(entityKey));
+                return Registry.ENTITY_TYPE.get(namespacedKey);
+            }
+        } else {
+            return null;
         }
     }
 
