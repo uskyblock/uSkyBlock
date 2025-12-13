c532617f1caaa8fd9933188c14c7e34d0960783e
minoneer <minoneer@gmail.com>
2025-12-13 10:57:15 +0100

    Add scheduler overloads to directly schedule BukkitTask. This ensures that the task internal state is updated, and methods such as cancel() work as intended.

--- uSkyBlock-Core/src/main/java/us/talabrek/ultimateskyblock/util/Scheduler.java
+++ uSkyBlock-Core/src/main/java/us/talabrek/ultimateskyblock/util/Scheduler.java
@@ -3,6 +3,7 @@ package us.talabrek.ultimateskyblock.util;
 import com.google.inject.Inject;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
 
@@ -23,23 +24,47 @@ public class Scheduler {
         return Bukkit.getScheduler().runTaskAsynchronously(plugin, runnable);
     }
 
+    public BukkitTask async(BukkitRunnable runnable) {
+        return runnable.runTaskAsynchronously(plugin);
+    }
+
     public BukkitTask async(Runnable runnable, Duration delay) {
         return Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, runnable, durationAsTicks(delay));
+    }
+
+    public BukkitTask async(BukkitRunnable runnable, Duration delay) {
+        return runnable.runTaskLaterAsynchronously(plugin, durationAsTicks(delay));
     }
 
     public BukkitTask async(Runnable runnable, Duration delay, Duration every) {
         return Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, runnable, durationAsTicks(delay), durationAsTicks(every));
     }
 
+    public BukkitTask async(BukkitRunnable runnable, Duration delay, Duration every) {
+        return runnable.runTaskTimerAsynchronously(plugin, durationAsTicks(delay), durationAsTicks(every));
+    }
+
     public BukkitTask sync(Runnable runnable) {
         return Bukkit.getScheduler().runTask(plugin, runnable);
+    }
+
+    public BukkitTask sync(BukkitRunnable runnable) {
+        return runnable.runTask(plugin);
     }
 
     public BukkitTask sync(Runnable runnable, Duration delay) {
         return Bukkit.getScheduler().runTaskLater(plugin, runnable, durationAsTicks(delay));
     }
 
+    public BukkitTask sync(BukkitRunnable runnable, Duration delay) {
+        return runnable.runTaskLater(plugin, durationAsTicks(delay));
+    }
+
     public BukkitTask sync(Runnable runnable, Duration delay, Duration every) {
         return Bukkit.getScheduler().runTaskTimer(plugin, runnable, durationAsTicks(delay), durationAsTicks(every));
+    }
+
+    public BukkitTask sync(BukkitRunnable runnable, Duration delay, Duration every) {
+        return runnable.runTaskTimer(plugin, durationAsTicks(delay), durationAsTicks(every));
     }
 }
