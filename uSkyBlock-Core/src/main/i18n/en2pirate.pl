a84e7438298dcf285bd521ee01fdabd5c6216dd1
minoneer <minoneer@gmail.com>
2026-03-02 22:59:09 +0100

    feature(i18n) Split translation keys into player-facing, admin-ops, and system-debug for easier translation prioritization (#105)
    * Refactor i18n to domain-split extraction and generated merge pipeline
    
    * Restructure translations into src/main/i18n with domain locale folders
    
    * Split locale catalogs into domain translation directories

---
+++ uSkyBlock-Core/src/main/i18n/en2pirate.pl
@@ -0,0 +1,93 @@
+use warnings;
+use strict;
+use utf8;
+binmode STDOUT, "utf8";
+my $filename = $ARGV[0] // 'keys.pot';
+my $output_file = $ARGV[1] // 'xx_PIRATE.po';
+open my $fp, '<:utf8', $filename or die "Unable to read $filename";
+open my $out, '>:encoding(utf8)', $output_file or die "Unable to write to $output_file";
+
+my %dict = (
+    "you've" => "ye've",
+    'your' => 'yer',
+    'you' => 'ye',
+    'am' => 'be',
+    '\sis' => ' be',
+    'are' => 'be',
+    'my' => 'me',
+    'them' => "'em",
+    'no' => 'nay',
+    'yes' => 'aye',
+    'and' => "an'",
+    'player' => 'pirate',
+    'players' => 'pirates',
+    'member' => 'mates',
+    'warning' => 'avast',
+    'error' => 'blimey',
+    'wrong' => 'awry',
+    'leader' => 'captain',
+    'saving' => "storin'",
+    'reward' => 'bounty',
+    #'level' => 'treasure',
+    #'levels' => 'treasure',
+    '\sisland' => ' hideout',
+    '\sislands' => ' hideouts',
+);
+my %part_dict = (
+    'ing' => "in'",
+    'on yer hideout' => 'in yer hideout',
+    'an hideout' => 'a hideout',
+);
+sub get_pirate {
+    my ($txt) = @_;
+    my $pirate = $txt;
+    utf8::encode($pirate);
+    print "txt: $txt\n";
+    my @parts = split /(<[^>]+>)/, $pirate;
+    for (my $i = 0; $i < scalar(@parts); $i++) {
+        next if $parts[$i] =~ /^<[^>]+>$/;
+        foreach my $key (keys %dict) {
+            my $value  = $dict{$key};
+            my $ukey   = ucfirst($key);
+            my $uvalue = ucfirst($value);
+
+            # Do not translate if the word is immediately preceded by '/'
+            $parts[$i] =~ s/(?<!\/)\b((§[0-9a-fklmor])?)\Q$key\E\b/$1$value/g;
+            $parts[$i] =~ s/(?<!\/)\b((§[0-9a-fklmor])?)\Q$ukey\E\b/$1$uvalue/g;
+        }
+        foreach my $key (keys %part_dict) {
+            $parts[$i] =~ s/$key/$part_dict{$key}/g;
+        }
+    }
+    $pirate = join('', @parts);
+    if ($pirate =~ m/\{0/) {
+        $pirate =~ s/''/'/g;
+        $pirate =~ s/'/''/g;
+    }
+    utf8::decode($pirate);
+    return $pirate;
+}
+
+while (my $line = <$fp>) {
+    chomp $line;
+    if (! ($line =~ m/#: .*$/)) {
+        # Skip line comments
+        $line =~ s/^\"Language: \\n/\"Language: xx-PIRATE\\n/g;
+        $line =~ s/^\"Last-Translator: FULL NAME <EMAIL\@ADDRESS>\\n/\"Last-Translator: R4zorax\\n/g;
+        print $out "$line\n";
+    }
+    if ($line =~ m/^msgid \"(?<txt>.*)\"$/) {
+        my $txt = $+{txt};
+        while (($line = <$fp>) =~ m/^\"(?<txt>.*)\"$/) {
+          $line = $+{txt};
+          $txt .= "\n" . $line;
+          print $out "\"$line\"\n";
+        }
+        my $pirate = get_pirate($txt);
+        print "pirate: $pirate\n";
+        $pirate =~ s/\n/\"\n\"/g;
+        print $out "msgstr \"$pirate\"\n";
+    }
+}
+close $fp;
+close $out;
